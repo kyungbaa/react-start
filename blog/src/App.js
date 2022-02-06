a4d9d28c37a78999b90b4f1a2d0372ee03988811
kyungbaa <93850460+kyungbaa@users.noreply.github.com>
2022-02-06 23:12:11 +0900

    시작

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,11 +1,19 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+let posts = "강남 고기 맛집";
 
 function App() {
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
+      </div>
+
+      <div className="list">
+        <h4>{posts}</h4>
+        <p>2월 17일 발행</p>
+        <hr />
       </div>
     </div>
   );

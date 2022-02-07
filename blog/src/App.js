0d1796db1d1dba07bb893a8a773f9a6444980e61
kyungbaa <93850460+kyungbaa@users.noreply.github.com>
2022-02-07 14:16:02 +0900

    sort

--- blog/src/App.js
+++ blog/src/App.js
@@ -13,7 +13,31 @@ function App() {
 
   let [좋아요, 좋아요변경] = useState(0);
   function 제목바꾸기() {
-    글제목변경(["여자코트 추천", "오늘의 집", "29cm 구매 후기"]);
+    /* 
+     reference date type 특징
+    수정된 [데이터]를 만들때 state 복사본을 만들어서 수정할 것 
+     let newArray = [...글제목]; > Deep copy   ...> 괄호 벗기기 ...[1,2,3] > 1,2,3
+     리액트 대 원칙: immutable date
+     -------------------------
+    1. 수정하고 싶은 state의 deep/shallow 카피본을 하나 생성합니다. 
+    2. 카피본을 입맛에 맞게 수정합니다. 
+    3. 카피본을 state변경함수()에 집어넣습니다. 
+     */
+    let newArray = [...글제목];
+    newArray[0] = "여자코트 추천";
+    글제목변경(newArray);
+  }
+
+  function 순서바꾸기() {
+    let newNameArray = [...글제목];
+    newNameArray.sort(function (a, b) {
+      if (newNameArray[a] < newNameArray[b]) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    글제목변경(newNameArray);
   }
 
   return (
@@ -22,6 +46,7 @@ function App() {
         <div>개발 Blog</div>
       </div>
       <button onClick={제목바꾸기}>수정 </button>
+      <button onClick={순서바꾸기}>타이틀 순 정렬</button>
       <div className="list">
         <h4>
           {글제목[0]}

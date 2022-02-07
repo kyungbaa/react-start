e9c7cc910d2ea1cecb3faecdf9f06d19a4ad8284
kyungbaa <93850460+kyungbaa@users.noreply.github.com>
2022-02-07 17:52:29 +0900

    반복문 사용

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-let posts = "강남 고기 맛집";
 
 function App() {
   let [글제목, 글제목변경] = useState([
@@ -11,7 +10,8 @@ function App() {
     "29cm 구매 후기",
   ]);
 
-  let [좋아요, 좋아요변경] = useState(0);
+  let [따봉, 따봉변경] = useState(0);
+  let [modal, modal변경] = useState(false); // on, off
   function 제목바꾸기() {
     /* 
      reference date type 특징
@@ -40,40 +40,81 @@ function App() {
     글제목변경(newNameArray);
   }
 
+  function 반복UI() {
+    let 어레이 = [];
+    for (let i = 0; i < 3; i++) {
+      어레이.push(<div>안녕</div>);
+    }
+    return 어레이;
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
+      {반복UI()}
+      {/* map 함수'=
+      반복할데이터.map(()=>{return<HTML>})
+       */}
+      {글제목.map(function (글) {
+        return (
+          <div className="list">
+            <h4>
+              {글}{" "}
+              <span
+                onClick={() => {
+                  따봉변경(따봉 + 1);
+                }}
+              >
+                👍
+              </span>
+              {따봉}
+            </h4>
+            <p>2월 17일 발행</p>
+            <hr />
+          </div>
+        );
+      })}
+
       <button onClick={제목바꾸기}>수정 </button>
       <button onClick={순서바꾸기}>타이틀 순 정렬</button>
-      <div className="list">
-        <h4>
-          {글제목[0]}
-          <span
-            onClick={() => {
-              좋아요변경(좋아요 + 1);
-            }}
-          >
-            👍
-          </span>
-          {좋아요}
-        </h4>
-        <p>2월 17일 발행</p>
-        <hr />
-      </div>
-      <div className="list">
-        <h4>{글제목[1]}</h4>
-        <p>2월 17일 발행</p>
-        <hr />
-      </div>
-      <div className="list">
-        <h4>{글제목[2]}</h4>
-        <p>2월 17일 발행</p>
-        <hr />
-      </div>
+
+      <button
+        onClick={() => {
+          modal변경(!modal);
+        }}
+      >
+        {" "}
+        열고닫는버튼{" "}
+      </button>
+      {modal === true ? <Modal /> : null}
     </div>
   );
 }
 
+function Modal() {
+  // component 유의사항
+  // 컴포넌트 만들 시 이름의 앞글자 대문자사용
+  // return()안에 div여러개 동일선상에서 두개 이상 불가 (많이 만들고 싶다면 큰 div안에 넣을것)
+  // 의미없는 큰 div를 만들기 싫을 경우 <></>를 사용한다.
+  // html을 한단어로 사용해서 만들 수 있음 덩어리째 관리하여 관리가 편해짐
+  /*
+  - 컴포넌트 장점 
+    반복적으로 출현하는 html들은 컴포넌트 만들기를 추천
+    사이트내에서 자주 변경되는 html UI들
+    다른 페이즐ㄹ 만들 때도 컴포넌트로 만듦 
+   - 컴포넌트 단점
+    state 쓸때 복잡함 
+   */
+  return (
+    <>
+      <div className="modal">
+        <h4>제목 </h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+    </>
+  );
+}
 export default App;

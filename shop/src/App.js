fcf9203117c2e04df0b2235688ae95677cb2d735
kyungbaa <93850460+kyungbaa@users.noreply.github.com>
2022-02-12 17:39:15 +0900

    shop component props

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+/*eslint-disable*/
+import React, { useState } from "react";
+import "./App.css";
+import "./index.css";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import Data from "./data";
 
 function App() {
+  let [acc, acc변경] = useState(Data);
+  // data.js의 데이터 바인딩 (data.js import 후 사용)
+  // 파일을 쪼갤 때 사용하는 import / export
+  // 다른파일로 분류할 때 사용한다 어떤 특정한 파일을 다른 파일에서 사용하게 하고 싶을 때는 export default  변수명[]
+  // export default는 파일 당 한번만 사용 가능하다
+  // 여러개의 변수를 export 하려면 export {name, name2} >> 대신 import할때 따로 작명이 아니라 import {name, name2} from "./data"; 이렇게 갖고 와야함
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="topbanner">
+        <div className="topbanner-contents">
+          <h1>Statement Jewellery</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea{" "}
+          </p>
+          <Button variant="dark">Dark</Button>{" "}
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          {/* props 전송법 
+          1. <자식 컴포넌트 보낼이름쀼쀼 = {전송할 state}
+          2. fuction 자식컴포넌트 (props){}
+          3. props. 보낼이름쀼쀼 사용 */}
+          {/* 나는 반복문을 자식 컴포넌트에서 생각했는데 아예 부모 컴포넌트에서 보낼때 각 해당번째 props를 보냄  */}
+
+          {
+            acc.map((accItem, i) => {
+              // accItem > acc 안에 있는 하나하나의 데이타
+              return <Card acc={accItem} key={i}></Card>; // = <Card acc={acc[i]} key={i}></Card>;
+            })
+            /*acc.map(function (accItem, i) {
+            return <Card acc={acc[i]}></Card>;
+          })*/
+          }
+        </div>
+      </div>
     </div>
   );
 }
 
+function Card(props) {
+  return (
+    <>
+      <div className="col-md-4 ">
+        <div className="container-contents">
+          <img src={props.acc.imgUrl}></img>
+          <h5>{props.acc.title}</h5>
+          <p>
+            {props.acc.content} & {props.acc.price}{" "}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
 export default App;
---
+++ shop/src/data.js
@@ -0,0 +1,85 @@
+export default [
+  {
+    id: 0,
+    title: "Black and Gold",
+    content: "Born in France",
+    imgUrl:
+      "https://images.unsplash.com/photo-1591348445640-572c959060be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+    price: 120000,
+  },
+
+  {
+    id: 1,
+    title: "Red Knit",
+    content: "Born in Seoul",
+    imgUrl:
+      "https://images.unsplash.com/photo-1613896640137-bb5b31496315?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    price: 110000,
+  },
+
+  {
+    id: 2,
+    title: "Jewelry Yordan",
+    content: "Born in the States",
+    imgUrl:
+      "https://images.unsplash.com/photo-1576053139778-7e32f2ae3cfd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    price: 130000,
+  },
+
+  {
+    id: 3,
+    title: "Wilson Text",
+    content: "Born in the States",
+    imgUrl:
+      "https://images.unsplash.com/photo-1623040593884-0044b3ce4ff9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+    price: 210000,
+  },
+  {
+    id: 4,
+    title: "Grey Yordan",
+    content: "Born in the States",
+    imgUrl:
+      "https://images.unsplash.com/photo-1585832641917-c40a6d4cec77?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=681&q=80",
+    price: 110000,
+  },
+  {
+    id: 5,
+    title: "Delicate dreams",
+    content: "Born in the States",
+    imgUrl:
+      "https://images.unsplash.com/photo-1626122738142-bbd66f671eca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    price: 80000,
+  },
+  {
+    id: 6,
+    title: "Grey Yordan",
+    content: "Born in the States",
+    imgUrl:
+      "https://images.unsplash.com/photo-1572987957529-46f6bd4880cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    price: 120000,
+  },
+  {
+    id: 7,
+    title: "Wilson Design ",
+    content: "Graphic Design ",
+    imgUrl:
+      "https://images.unsplash.com/photo-1626122780071-c09d403b8e32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    price: 230000,
+  },
+  {
+    id: 8,
+    title: "Jewelry Shadows",
+    content: "Born in the States",
+    imgUrl:
+      "https://images.unsplash.com/photo-1561828995-aa79a2db86dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    price: 230000,
+  },
+  {
+    id: 9,
+    title: "Flower Images",
+    content: "Flatlays",
+    imgUrl:
+      "https://images.unsplash.com/photo-1599459182681-c938b7f65af0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80",
+    price: 150000,
+  },
+];
